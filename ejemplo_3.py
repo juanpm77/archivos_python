1032773b3c3494815e4228a958519def16e44c9c
juanpm77 <124220122+juanpm77@users.noreply.github.com>
2023-04-01 21:55:49 -0300

    Resolución
    Se creo la función para calcular el promedio por género.

--- ejemplo_3.py
+++ ejemplo_3.py
@@ -20,10 +20,36 @@ def altura_promedio(genero):
 
     # Profe:
     # - Leer el archivo CSV
+    with open('alturas.csv') as csvfile:
+        datos_altura = list(csv.DictReader(csvfile))
+
     # - Recorrer todas las filas del archivo CSV
     # - En cada iteración obtenga la altura del genero objetivo
+    
+    lista_alturas = []
+    for i in range(len(datos_altura)):
+        if genero == datos_altura[i]['genero']:
+            altura = float(datos_altura[i]['altura'])
+            lista_alturas.append(altura)
+
+
+    promedio = sum(lista_alturas) / len(lista_alturas)
+    print(f'El promedio con listas de la altura del genero: {genero} es de: {promedio}')
+
     # - Acumule el valor y la cantidad para realizar
     #   el promedio al terminar el bucle
+
+    sumatoria_alturas = 0
+    cantidad_alturas = 0
+
+    for i in range(len(datos_altura)):
+        if genero == datos_altura[i]['genero']:
+            altura = float(datos_altura[i]['altura'])
+            sumatoria_alturas += altura
+            cantidad_alturas += 1
+
+    promedio = sumatoria_alturas / cantidad_alturas
+    print(f'El promedio con sumatoria de la altura del genero {genero}es de {promedio}')
 
     # --- Comience aquí a desarrollar su código ---
 
